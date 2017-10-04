c94d1b19948af03d0641e31eea0650b897db7f84
Isaac Lyman <isaaclyman@gmail.com>
2017-10-03 21:58:19 -0600

    Make 'npm run dev' open /app.html

--- build/dev-server.js
+++ build/dev-server.js
@@ -75,7 +75,7 @@ devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
-    opn(uri)
+    opn(uri + '/app.html')
   }
   _resolve()
 })

1224abf663e2b4f00bddea926797137fa74c623e
Isaac Lyman <isaaclyman@gmail.com>
2017-10-07 09:25:56 -0600

    Add Tools page component

---
+++ src/components/tools/tools.page.vue
@@ -0,0 +1,21 @@
+<template>
+  <h4>This page is coming soon.</h4>
+</template>
+
+<script>
+export default {
+  beforeCreate () {
+
+  },
+  components: {},
+  computed: {},
+  data () {
+    return {}
+  },
+  props: {}
+}
+</script>
+
+<style scoped>
+
+</style>

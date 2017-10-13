b153c71187ef3557255e718591128b269f42417a
Isaac Lyman <isaaclyman@gmail.com>
2017-10-12 21:19:31 -0600

    Start using Express.js as a web server

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -91,10 +91,6 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
-      },
-      {
-        from: path.resolve(__dirname, '../landing'),
-        to: config.build.assetsRoot
       }
     ])
   ]
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Serve static files
+app.use('/static', express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/app.html'))
+})
+
+app.listen(3000, () => {
+  console.log('Express listening on port 3000')
+})
+
+module.exports = app

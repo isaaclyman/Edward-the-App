ddc7b13a3a846b016f9f1eedc655139ba9488392
Isaac Lyman <isaaclyman@gmail.com>
2017-10-13 15:49:13 -0600

    Use PORT env var for server if available

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/app', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/app.html'))
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Express listening on port 3000')
 })
 

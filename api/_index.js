6240d6234aca9454963f907be0ab05f82ab51266
Isaac Lyman <isaaclyman@gmail.com>
2017-11-01 12:52:21 -0600

    Auto redirect to HTTPS on secure pages when not on localhost

--- api/_index.js
+++ api/_index.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = function (app, passport, db) {
   // Serve auth pages
-  app.get('/auth', (req, res) => {
+  app.get('/auth', httpsMiddleware, (req, res) => {
     req.logout()
     res.sendFile(path.join(__dirname, '../dist/auth.html'))
   })
@@ -10,7 +10,7 @@ module.exports = function (app, passport, db) {
   app.get('/login', (req, res) => { res.redirect('/auth') })
 
   // Serve main app
-  app.get('/app', isLoggedIn, (req, res) => {
+  app.get('/app', httpsMiddleware, isLoggedIn, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/app.html'))
   })
 
@@ -25,4 +25,16 @@ function isLoggedIn (req, res, next) {
   }
 
   res.redirect('/auth')
+}
+
+function httpsMiddleware (req, res, next) {
+  const host = req.get('host')
+  const isLocal = host.startsWith('localhost') || host.startsWith('127.0.0.1')
+
+  if (req.protocol !== 'https' && !isLocal) {
+    res.redirect(`https://${host}${req.originalUrl}`)
+    return
+  }
+
+  return next()
 }

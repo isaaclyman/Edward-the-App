9727748888f983d6b4241cbf64683b8e92500c5d
Isaac Lyman <isaaclyman@gmail.com>
2017-11-10 10:10:26 -0700

    Proxy API, not entire site, so full-stack dev is possible with hot reload

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/': {
+      '/api': {
         target: 'http://127.0.0.1:3000',
         changeOrigin: true
       }

e8dc208049ac4b14da7e92052210782e2727ee68
Isaac Lyman <isaaclyman@gmail.com>
2017-11-15 07:57:57 -0700

    Test deleting/updating/arranging a topic

--- test/api/topic.js
+++ test/api/topic.js
@@ -69,6 +69,72 @@ test('add topics', async t => {
   return checkTopics(t, app, doc.id, topics => {
     t.is(topics.length, 2)
     compareTopics(t, doc.id, topics[0], topic1)
-    compareTopics(t, doc.id, topics[1], topic2)    
+    compareTopics(t, doc.id, topics[1], topic2)
+  })
+})
+
+test('arrange topics', async t => {
+  const topic1 = await addTopic(app, doc.id, 'Test1')
+  const topic2 = await addTopic(app, doc.id, 'Test2')
+
+  await (
+    app.post(route('topic/arrange'))
+    .send({
+      fileId: doc.id,
+      topicIds: [topic2.topicId, topic1.topicId]
+    })
+    .expect(200)
+  )
+
+  return checkTopics(t, app, doc.id, topics => {
+    t.is(topics.length, 2)
+    compareTopics(t, doc.id, topics[0], topic2)
+    compareTopics(t, doc.id, topics[1], topic1)
+  })
+})
+
+test('delete topic', async t => {
+  const topic1 = await addTopic(app, doc.id, 'Test1')
+  const topic2 = await addTopic(app, doc.id, 'Test2')
+
+  await (
+    app.post(route('topic/delete'))
+    .send({
+      fileId: doc.id,
+      topicId: topic1.topicId
+    })
+    .expect(200)
+  )
+
+  return checkTopics(t, app, doc.id, topics => {
+    t.is(topics.length, 1)
+    compareTopics(t, doc.id, topics[0], topic2)
+  })
+})
+
+test('update topic', async t => {
+  const topic1 = await addTopic(app, doc.id, 'Test1')
+  const topic2 = await addTopic(app, doc.id, 'Test2')
+
+  const newTopic = {
+    fileId: doc.id,
+    topicId: topic1.topicId,
+    topic: {
+      archived: true,
+      id: topic1.topicId,
+      title: 'Test1-updated'
+    }
+  }
+
+  await (
+    app.post(route('topic/update'))
+    .send(newTopic)
+    .expect(200)
+  )
+
+  return checkTopics(t, app, doc.id, topics => {
+    t.is(topics.length, 2)
+    compareTopics(t, doc.id, topics[0], newTopic)
+    compareTopics(t, doc.id, topics[1], topic2)
   })
 })

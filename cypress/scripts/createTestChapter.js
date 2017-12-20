d0b80f56c42daa2b8bf6020d419f1b608c26adbb
Isaac Lyman <isaaclyman@gmail.com>
2017-12-20 10:37:50 -0700

    Update cypress and convert cypress scripts to knex

--- cypress/scripts/createTestChapter.js
+++ cypress/scripts/createTestChapter.js
@@ -1,39 +1,30 @@
 require('./setUp')
 const util = require('../../test/_util')
-const sequelize = require('../../db')
+const knex = require('../../db')
 const guid = require('uuid/v1')
 
-const chapId = guid()
-let docId, docGuid
-let userId
+const chapGuid = guid()
 
-sequelize.query(`
-    SELECT id FROM users WHERE email = '${util.user.email}' LIMIT 1
-`, { type: sequelize.QueryTypes.SELECT }
-).then(([{ id }]) => {
-    userId = id
-    const query = `
-    SELECT id, guid FROM documents WHERE "userId" = ${userId} LIMIT 1
-    `
-    return sequelize.query(query, { type: sequelize.QueryTypes.SELECT }).then(([{ id, guid }]) => {
-        docId = id
-        docGuid = guid
-    })
+const userQuery = `(SELECT id FROM users WHERE email = '${util.user.email}' LIMIT 1)`
+const userId = knex.raw(userQuery)
+const docId = knex.raw(`(SELECT id FROM documents WHERE user_id = ${userQuery} LIMIT 1)`)
+
+knex('chapters').insert({
+  archived: false,
+  content: null,
+  guid: chapGuid,
+  title: 'test',
+  'user_id': userId,
+  'document_id': docId
 }).then(() => {
-    const query = `
-    INSERT INTO chapters (archived, content, guid, title, "createdAt", "updatedAt", "userId", "documentId")
-    VALUES (false, NULL, '${chapId}', 'test', current_timestamp, current_timestamp, ${userId}, ${docId})
-    `
-    return sequelize.query(query, { type: sequelize.QueryTypes.INSERT })
+  return knex('chapter_orders').insert({
+    order: JSON.stringify([chapGuid]),
+    'document_id': docId,
+    'user_id': userId
+  })
 }).then(() => {
-    const query = `
-    INSERT INTO chapter_orders ("ownerGuid", "order", "createdAt", "updatedAt", "userId")
-    VALUES ('${docGuid}', ARRAY['${chapId}'::uuid], current_timestamp, current_timestamp, ${userId})
-    `
-    return sequelize.query(query, { type: sequelize.QueryTypes.INSERT })
-}).then(() => {
-    process.exit()
+  process.exit()
 }, err => {
-    console.error(err)
-    process.exit(1)
+  console.error(err)
+  process.exit(1)
 })
--- cypress/scripts/createTestDocument.js
+++ cypress/scripts/createTestDocument.js
@@ -1,29 +1,25 @@
 require('./setUp')
 const util = require('../../test/_util')
-const sequelize = require('../../db')
+const knex = require('../../db')
 const guid = require('uuid/v1')
 
-const docId = guid()
-let userId
+const docGuid = guid()
 
-sequelize.query(`
-    SELECT id FROM users WHERE email = '${util.user.email}'
-`, { type: sequelize.QueryTypes.SELECT }).then(([{ id }]) => {
-    userId = id
-    const query = `
-    INSERT INTO documents (guid, name, "createdAt", "updatedAt", "userId")
-    VALUES ('${docId}', 'test', current_timestamp, current_timestamp, ${userId})
-    `
-    return sequelize.query(query, { type: sequelize.QueryTypes.INSERT })
+const userQuery = `(SELECT id FROM users WHERE email = '${util.user.email}' LIMIT 1)`
+const userId = knex.raw(userQuery)
+
+knex('documents').insert({
+  guid: docGuid,
+  name: 'test',
+  'user_id': userId
 }).then(() => {
-    const query = `
-    INSERT INTO document_orders ("order", "createdAt", "updatedAt", "userId")
-    VALUES (ARRAY['${docId}'::uuid], current_timestamp, current_timestamp, ${userId})
-    `
-    return sequelize.query(query, { type: sequelize.QueryTypes.INSERT })
+  return knex('document_orders').insert({
+    order: JSON.stringify([docGuid]),
+    'user_id': userId
+  })
 }).then(() => {
-    process.exit()
+  process.exit()
 }, err => {
-    console.error(err)
-    process.exit(1)
+  console.error(err)
+  process.exit(1)
 })

104265c386960fb315ff010e95a2580d631c4ab6
Isaac Lyman <isaaclyman@gmail.com>
2017-12-21 13:39:04 -0700

    Test limited and premium users on plan Cypress test

--- cypress/integration/plan.js
+++ cypress/integration/plan.js
@@ -1,19 +1,33 @@
 import { user } from '../../test/_util'
 import { createTestChapter, createTestDocument, createTestUser, deleteTestUser, logIn, makeTestUserPremium } from '../scripts/_util'
 
-describe('the plans page', () => {
+const tests = isPremium => () => {
   before(() => {
     deleteTestUser(cy)
     createTestUser(cy)
-    makeTestUserPremium(cy)
-    createTestDocument(cy)
-    createTestChapter(cy)
+
+    if (isPremium) {
+      makeTestUserPremium(cy)
+      createTestDocument(cy, isPremium)
+      createTestChapter(cy, isPremium)
+    }
+  })
+
+  beforeEach(() => {
     logIn(cy, user.email, user.password)
     cy.visit('/app.html#/plan')
     cy.get('select.file-dropdown').select('test')
+
+    if (!isPremium) {
+      const docId = createTestDocument(cy, isPremium)
+      createTestChapter(cy, isPremium, docId)
+    }
   })
 
   it('has a plans list', () => {
     cy.get('h3').contains('Plans')
   })
-})
+}
+
+describe('the plans page (limited)', tests(false))
+describe('the plans page (premium)', tests(true))

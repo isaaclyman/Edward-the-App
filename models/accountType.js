0986633646088c2b3b7e2bf22cface4ae43fba6f
Isaac Lyman <isaaclyman@gmail.com>
2018-02-06 09:34:38 -0700

    Move webpack/babel/eslint packages to devDependencies and upgrade; make accountType exportable ES5-friendly; build latest

--- models/accountType.js
+++ models/accountType.js
@@ -1,16 +1,18 @@
-const createAccountType = (name, displayName, description) => ({ name, displayName, description })
+const createAccountType = function (name, displayName, description) {
+  return { name: name, displayName: displayName, description: description }
+}
 
 const accountTypes = {
   DEMO: createAccountType('DEMO', 'Demo Account',
-    `This is a demo. Your data is not stored and will be lost when you close the current tab.`),
+    'This is a demo. Your data is not stored and will be lost when you close the current tab.'),
   LIMITED: createAccountType('LIMITED', 'Limited Account',
-    `All data is stored insecurely on your computer's hard drive and may be lost if your browsing data is cleared.
-    Your maximum storage limit is probably 5MB (about 2,500 pages).`),
+    'All data is stored insecurely on your computer\'s hard drive and may be lost if your browsing data is cleared.\
+    Your maximum storage limit is probably 5MB (about 2,500 pages).'),
   PREMIUM: createAccountType('PREMIUM', 'Premium Account',
-    `Your data is stored on our servers. Your storage limit is 20MB (about 10,000 pages).`),
+    'Your data is stored on our servers. Your storage limit is 20MB (about 10,000 pages).'),
   GOLD: createAccountType('GOLD', 'Gold Account',
-    `Your data is stored on our servers. Your storage limit is 250MB (about 125,000 pages).`),
-  ADMIN: createAccountType('ADMIN', 'Admin Account', `You know who you are.`)
+    'Your data is stored on our servers. Your storage limit is 250MB (about 125,000 pages).'),
+  ADMIN: createAccountType('ADMIN', 'Admin Account', 'You know who you are.')
 }
 
 module.exports = accountTypes

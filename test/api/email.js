32cbf4c48d1cd26cb91a8adfa9601a5c503375ee
Isaac Lyman <isaaclyman@gmail.com>
2018-02-09 08:28:58 -0700

    Update ava and fix email integration test

--- test/api/email.js
+++ test/api/email.js
@@ -1,15 +1,18 @@
-import { test } from '../_imports'
+import { serverReady, test } from '../_imports'
 import Email from '../../api/email.helper'
 
 test('An email can be sent', async t => {
+  await serverReady
   const email = new Email(
     ['trash@edwardtheapp.com'],
     'Integration test by test/api/email.js',
-    `This is a test email and was probably received in error.
-    If you see it more than once, contact support@edwardtheapp.com.`
+    'This is a test email and was probably received in error.' +
+    '\nIf you see it more than once, contact support@edwardtheapp.com.'
   )
   
-  await email.send().then(info => {
+  return email.send().then(info => {
     t.truthy(info.messageId)
-  }, t.fail)
+  }, err => {
+    t.fail(`Email failed to send: ${err}`)
+  })
 })

a8d0845044b2315034bbda97215111c55cb16341
Isaac Lyman <isaaclyman@gmail.com>
2018-02-14 13:03:54 -0700

    Fix tests for demo user access to user APIs

--- test/api/user.demo.js
+++ test/api/user.demo.js
@@ -17,20 +17,17 @@ let app
 test.beforeEach('set up demo user', async t => {
   app = getPersistentAgent()
   await deleteTestUser()
-  await createTestUser()
+  await createTestUser(app)
   await makeTestUserDemo()
   await serverReady
 })
 
-test(`can't modify the demo account using the APIs`, async t => {
-  await [
-    route('user/send-verify-link'),
-    route('user/verify'),
-    route('user/email'),
-    route('user/password'),
-    route('user/upgrade'),
-    route('user/update-payment')
-  ].forEach(async endpoint => {
-    await app.post(endpoint).expect(500)
-  })
+test(`can't modify demo account using APIs`, async t => {
+  await app.post(route('user/send-verify-link')).expect(500)
+  await app.post(route('user/verify')).expect(500)
+  await app.post(route('user/email')).expect(500)
+  await app.post(route('user/password')).expect(500)
+  await app.post(route('user/upgrade')).expect(500)
+  await app.post(route('user/update-payment')).expect(500)
+  await app.post(route('user/send-reset-password-link')).expect(500)
 })

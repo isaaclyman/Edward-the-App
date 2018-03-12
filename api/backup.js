147fa6efb6e72bd0e22aa15d1d7360432a64e270
Isaac Lyman <isaaclyman@gmail.com>
2018-03-12 10:31:06 -0600

    Add backup APIs for single doc import/export

--- api/backup.js
+++ api/backup.js
@@ -2,6 +2,27 @@ const { addDocument, deleteDocument, getDocuments, saveAllContent } = require('.
 const { getChapters } = require('./chapter')
 const { getTopics } = require('./topic')
 const { getPlans } = require('./plan')
+const find = require('lodash/find')
+
+const getDocExport = (db, userId, documentGuid) => {  
+  return Promise.all([
+    getDocuments(db, userId).then(docs => {
+      return find(docs, doc => doc.guid === documentGuid)
+    }),
+    getChapters(db, userId, docGuid),
+    getTopics(db, userId, docGuid),
+    getPlans(db, userId, docGuid)
+  ]).then(([doc, chapters, topics, plans]) => {
+    if (!doc) {
+      throw new Error('Could not find document.')
+    }
+
+    doc.chapters = chapters
+    doc.topics = topics
+    doc.plans = plans
+    return doc
+  })
+}
 
 const getFullExport = (db, userId) => {
   return getDocuments(db, userId).then(docs => {
@@ -19,6 +40,12 @@ const getFullExport = (db, userId) => {
         return doc
       })
     }))
+  })
+}
+
+const importDoc = (db, userId, doc) => {
+  return addDocument(db, userId, doc).then(() => {
+    return saveAllContent(db, userId, doc.guid, doc.chapters, doc.topics, doc.plans)
   })
 }
 
@@ -72,11 +99,51 @@ const registerApis = function (app, passport, db, isPremiumUser) {
     })
   })
 
+  // POST { guid, name, chapters, topics, plans }
+  app.post(route('import/document'), isPremiumUser, (req, res, next) => {
+    const userId = req.user.id
+    const newDoc = req.body
+    
+    let oldDoc
+    getDocExport(db, userId, newDoc.guid).then(doc => {
+      oldDoc = doc
+    }).then(() =>
+      deleteDocument(db, userId, newDoc.guid)
+    ).then(() =>
+      importDoc(db, userId, newDoc)
+    ).then(() => {
+      res.status(200).send()
+    }, err => {
+      console.error('[DOCUMENT IMPORT ERROR.]', err)
+      deleteDocument(db, userId, newDoc.guid).then(() =>
+        importDoc(db, userId, oldDoc)
+      ).then(() => {
+        res.status(500).send(`[DOCUMENT IMPORT REVERTED.] ${err}`)
+      }, revertErr => {
+        console.error('[DOCUMENT IMPORT REVERT ERROR.]', revertErr)
+        res.status(500).send(`[DOCUMENT IMPORT REVERT FAILED.] ${revertErr}`)
+      })
+    })
+  })
+
   // GET
   app.get(route('export'), isPremiumUser, (req, res, next) => {
     const userId = req.user.id
     getFullExport(db, userId).then(docs => {
       res.status(200).send(docs)
+    }, err => {
+      console.error(err)
+      res.status(500).send(err)
+    })
+  })
+
+  // GET
+  app.get(route('export/document/:documentGuid'), isPremiumUser, (req, res, next) => {
+    const userId = req.user.id
+    const documentGuid = req.params.documentGuid
+
+    getDocExport(db, userId, documentGuid).then(doc => {
+      res.status(200).send(doc)
     }, err => {
       console.error(err)
       res.status(500).send(err)

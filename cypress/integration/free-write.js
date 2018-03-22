e99654ef94147b23f9f4b5f07bd8b6058fb0f183
Isaac Lyman <isaaclyman@gmail.com>
2018-03-22 10:55:07 -0600

    Add Cypress tests for free write workshop

---
+++ cypress/integration/free-write.js
@@ -0,0 +1,118 @@
+import { user } from '../../test/_util'
+import {
+  createTestChapter, createTestDocument, createTestUser, deleteTestUser, logIn, makeTestUserPremium
+} from '../scripts/_util'
+
+describe('the free write workshop', () => {
+  before(() => {
+    deleteTestUser(cy)
+    createTestUser(cy)
+    makeTestUserPremium(cy)
+    return createTestDocument(cy, true).then(() => createTestChapter(cy, true))
+  })
+
+  beforeEach(() => {
+    logIn(cy, user.email, user.password)
+  })
+
+  it('should navigate to the free write workshop from the workshops modal', () => {
+    cy.visit('/app.html#/write')
+    cy.get('select.document-dropdown').select('test')
+    cy.get('.main-menu').find('.main-menu--button').contains('Workshop').click()
+    cy.get('.swal-modal').find('.workshop').contains('Free Writing').click()
+    cy.url().should('contain', '/workshop/free-write')
+  })
+
+  describe('the free write workshop', () => {
+    beforeEach(() => {
+      cy.clearLocalStorage()
+      cy.visit('/app.html#/workshop/free-write')
+      cy.get('select.document-dropdown').select('test')
+    })
+
+    const typeContent = (cy, identifier, content) => {
+      const clock = cy.clock()
+      cy.get(identifier).clear()
+
+      if (content) {
+        cy.get(identifier).type(content)
+      }
+
+      cy.get(identifier).should('contain', content)
+      clock.tick(1000)
+    }
+  
+    it('should allow setting a word limit and track word count', () => {
+      cy.get('.limit-option label').contains('word limit').click()
+      cy.get('.set-limit input').clear().type('15')
+      cy.get('.begin .button-green').click()
+
+      typeContent(cy, '.ql-editor', `Testing word limit.`)
+      cy.get('.counter').should('contain', '3 of 15 words written')
+      typeContent(cy, '.ql-editor', `la${' la'.repeat(14)}.`)
+      cy.get('.counter').should('contain', '15 of 15 words written')
+      cy.get('.done .button-green').click()
+    })
+  
+    it('should allow setting a time limit and track minute count', () => {
+      cy.get('.limit-option label').contains('time limit').click()
+      cy.get('.set-limit input').clear().type('1')
+
+      const clock = cy.clock()
+      cy.get('.begin .button-green').click()
+      typeContent(cy, '.ql-editor', 'Testing time limit')
+      cy.get('.counter').should('contain', 'minute left')      
+      clock.tick(30 * 1000)
+      cy.get('.counter').should('contain', 'seconds left')
+      clock.tick(31 * 1000)
+      cy.get('.counter').should('contain', 'Timer expired')
+      cy.get('.done .button-green').click()
+    })
+
+    it('should allow continuing without a limit', () => {
+      cy.get('.limit-option label').contains('No limit').click()
+      cy.get('.begin .button-green').click()
+      cy.get('.limit-option').should('not.be.visible')
+      cy.get('.counter').should('contain', 'Set a limit')
+      typeContent(cy, '.ql-editor', 'Testing no limit')
+      const clock = cy.clock()
+      cy.get('.done .button-green').click()
+      clock.tick(2000)
+      cy.get('.ql-editor').should('not.be.visible')
+      cy.get('.finished').should('contain', 'Saved')
+    })
+
+    it('should indicate that the workshop was deleted if no content was entered', () => {
+      cy.get('.limit-option label').contains('No limit').click()
+      cy.get('.begin .button-green').click()
+      cy.get('.limit-option').should('not.be.visible')
+      cy.get('.counter').should('contain', 'Set a limit')
+      typeContent(cy, '.ql-editor', '') // Intentionally empty
+      const clock = cy.clock()
+      cy.get('.done .button-green').click()
+      clock.tick(2000)
+      cy.get('.ql-editor').should('not.be.visible')
+      cy.get('.finished').should('contain', 'Deleted')
+    })
+  
+    it('should edit the same workshop if the page is refreshed', () => {
+      
+    })
+  
+    it('should show a new workshop if the workshop is completed', () => {
+  
+    })
+  
+    it('should show completed workshops on the Workshops column of the composer', () => {
+  
+    })
+  
+    it('should allow archiving and restoring a workshop from the compose page', () => {
+  
+    })
+  
+    it('should allow deleting a workshop forever from the compose page', () => {
+      
+    })
+  })
+})

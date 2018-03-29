659f96199af8da1d534d5752630567849cc73535
Isaac Lyman <isaaclyman@gmail.com>
2018-03-28 19:57:37 -0600

    Reduce size of auth bundle significantly by only putting packages used by 2+ entry points in the vendor bundle

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -88,21 +88,22 @@ var webpackConfig = merge(baseWebpackConfig, {
       chunks: ['vendor', 'manifest', 'admin'],
       chunksSortMode: 'dependency'
     }),
-    // keep module.id stable when vender modules does not change
+    // keep module.id stable when vendor modules do not change
     new webpack.HashedModuleIdsPlugin(),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
+      minChunks: 2
+      // function (module, count) {
+      //   // any required modules inside node_modules are extracted to vendor
+      //   return (
+      //     module.resource &&
+      //     /\.js$/.test(module.resource) &&
+      //     module.resource.indexOf(
+      //       path.join(__dirname, '../node_modules')
+      //     ) === 0
+      //   )
+      // }
     }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated

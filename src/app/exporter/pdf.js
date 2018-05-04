66c782b558d0b6878ddffbf4a33d1d03bd57b87d
Isaac Lyman <isaaclyman@gmail.com>
2018-05-04 07:41:08 -0600

    Factor out the Delta array rejiggerer into a shared utility

--- src/app/exporter/pdf.js
+++ src/app/exporter/pdf.js
@@ -1,6 +1,7 @@
 import pdfMake from 'pdfmake/build/pdfmake'
 import pdfFonts from 'pdfmake/build/vfs_fonts.js'
 import FileSaver from 'file-saver'
+import { getStyledArrayFromChapters } from '../../../utilities'
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs
 
@@ -49,75 +50,7 @@ export const chaptersToPdf = (title, chapters) => {
       }
     }
 
-    const splitContent = []
-
-    function getNewLine () {
-      return {
-        text: [],
-        style: []
-      }
-    }
-
-    function getNewSegment (text, style) {
-      return {
-        text: text || '',
-        style: style || []
-      }
-    }
-
-    for (const chapter of chapters) {
-      splitContent.push({
-        text: chapter.title,
-        style: 'chapterHeading',
-        pageBreak: 'before'
-      })
-
-      let currentLine = getNewLine()
-      const ops = (chapter.content && chapter.content.ops) || []
-      for (const op of ops) {
-        const insert = op.insert
-        const { styles, lineStyles } = getStyles(op.attributes)
-
-        if (lineStyles.length) {
-          currentLine.style.push(...lineStyles)
-        }
-
-        if (insert === '\n') {
-          splitContent.push(currentLine)
-          currentLine = getNewLine()
-          continue
-        }
-
-        if (insert.includes('\n')) {
-          const [first, ...lines] = insert.split('\n')
-          if (first) {
-            const segment = getNewSegment(first, styles)
-            currentLine.text.push(segment)
-          }
-
-          splitContent.push(currentLine)
-          currentLine = getNewLine()
-
-          for (let ix = 0; ix < lines.length - 1; ix++) {
-            const line = lines[ix]
-            const segment = getNewSegment(line, styles)
-            currentLine.text.push(segment)
-
-            splitContent.push(currentLine)
-            currentLine = getNewLine()
-          }
-
-          const last = lines[lines.length - 1]
-          const segment = getNewSegment(last, styles)
-          currentLine.text.push(segment)
-
-          continue
-        }
-
-        const segment = getNewSegment(insert, styles)
-        currentLine.text.push(segment)
-      }
-    }
+    const splitContent = getStyledArrayFromChapters(chapters)
 
     let counter = 1
     splitContent.forEach(content => {
@@ -146,41 +79,4 @@ export const chaptersToPdf = (title, chapters) => {
       resolve()
     })
   })
-}
-
-const listMap = {
-  'bullet': 'ul',
-  'ordered': 'ol'
-}
-function getStyles (attributes) {
-  const styles = []
-  const lineStyles = []
-
-  if (!attributes) {
-    return { styles, lineStyles }
-  }
-
-  for (const attr in attributes) {
-    if (['bold', 'italic', 'underline', 'strike'].includes(attr)) {
-      styles.push(attr)
-      continue
-    }
-
-    if (attr === 'header') {
-      lineStyles.push(`h${attributes[attr]}`)
-      continue
-    }
-
-    if (attr === 'blockquote') {
-      lineStyles.push('blockquote')
-      continue
-    }
-
-    if (attr === 'list') {
-      lineStyles.push(listMap[attributes[attr]])
-      continue
-    }
-  }
-
-  return { styles, lineStyles }
 }

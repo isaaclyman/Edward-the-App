06aa7cbe008f0ff3e6cb40c6c2d1eb4bed82397c
Isaac Lyman <isaaclyman@gmail.com>
2018-05-29 10:20:05 -0600

    Add more plot prompts

--- src/app/workshops/resources/plot-prompts.js
+++ src/app/workshops/resources/plot-prompts.js
@@ -1,4 +1,11 @@
 export default [
-  `Intrigue is when one character knows something that another character doesn't. `,
-  `Most plots are the `
+  `Intrigue is when one character keeps a harmful secret from another. Write a scene that creates intrigue.`,
+  `Most plots are a series of structured surprises. Write a scene that surprises the reader.`,
+  `You can raise the stakes by putting something a character cares about at risk. Write a scene that raises the stakes.`,
+  `Dramatic irony is created when the reader knows something important that a character does not. Write a scene that creates dramatic irony.`,
+  `Suspense is when the reader wonders anxiously what will happen next. Write a scene where the outcome of a decision, a natural event, or an interaction is urgent and uncertain.`,
+  `Plots often weave together moments of action and moments of reflection. Determine whether your last scene involved more action or more reflection, then write a scene that does the opposite.`,
+  `A character's first attempt to solve a problem is rarely successful. Write a scene where someone tries and fails to solve a problem.`,
+  `Perverseness is when a character willfully makes the wrong choice or makes things worse for themself. Write a scene where a character does something perverse.`,
+  ``
 ]

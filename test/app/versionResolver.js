569f558a6a0a755edcad703cd227a9769f49636e
Isaac Lyman <isaaclyman@gmail.com>
2018-08-14 11:46:51 -0600

    Add advanced tests for VersionResolver.getMostRecentEach()

--- test/app/versionResolver.js
+++ test/app/versionResolver.js
@@ -2,17 +2,17 @@ import clone from 'lodash/clone'
 import { test } from '../_imports'
 import VersionResolver from '../../src/app/shared/versionResolver'
 
-const wait = async () => await new Promise(resolve => setTimeout(resolve, 100))
+const wait = async () => await new Promise(resolve => setTimeout(resolve, 50))
 
 test('timestamps increment', async t => {
   t.plan(1)
 
-  var obj = {}
+  var obj1 = {}
   var obj2 = {}
-  VersionResolver.timestamp(obj)
+  VersionResolver.timestamp(obj1)
   await wait()
   VersionResolver.timestamp(obj2)
-  t.true(VersionResolver.getTimestamp(obj2) > VersionResolver.getTimestamp(obj))
+  t.true(VersionResolver.getTimestamp(obj2) > VersionResolver.getTimestamp(obj1))
 })
 
 test('timestampEach timestamps each element', t => {
@@ -33,4 +33,56 @@ test('getMostRecent returns the most recently timestamped element or default', a
   t.is(VersionResolver.getMostRecent(newObj, oldObj), newObj)
   t.is(VersionResolver.getMostRecent(oldObj, sameObj), oldObj)
   t.is(VersionResolver.getMostRecent(sameObj, oldObj), sameObj)
+})
+
+test('getMostRecentEach merges two arrays, marking exclusive items as deleted', t => {
+  var [obj1, obj2, obj3] = [{id: 1}, {id: 2}, {id: 3}]
+  var arr1 = [obj1, obj2]
+  var arr2 = [obj1, obj3]
+  
+  var result = VersionResolver.getMostRecentEach(arr1, arr2, obj => obj.id, obj => { obj.deleted = true })
+  
+  t.true(result.some(obj => obj.id === 1))
+  t.true(result.some(obj => obj.id === 2))
+  t.true(result.some(obj => obj.id === 3))
+
+  t.falsy(result.find(obj => obj.id === 1).deleted)
+  t.true(result.find(obj => obj.id === 2).deleted)
+  t.true(result.find(obj => obj.id === 3).deleted)
+})
+
+test('getMostRecentEach merges two arrays, keeping the most recent version of each element', async t => {
+  var [obj1, obj2, obj3, obj4, obj5, obj6] = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}]
+  var [new2, new3, new4] = [{id: 2}, {id: 3}, {id: 4}].map(obj => {
+      obj.isNew = true
+      return obj
+    })
+  
+  var fullArr = [obj1, obj2, obj3, obj4, obj5, obj6]
+  for (var obj of fullArr) {
+    await wait()
+    VersionResolver.timestamp(obj)
+  }
+
+  var newArr = [new2, new3, new4]
+  for (var obj of newArr) {
+    await wait()
+    VersionResolver.timestamp(obj)
+  }
+
+  var arr1 = [obj1, new2, obj4, obj5]
+  var arr2 = [obj1, obj2, new3, new4, obj6]
+
+  var result = VersionResolver.getMostRecentEach(arr1, arr2, obj => obj.id, () => {})
+
+  for (var obj of fullArr) {
+    t.true(result.some(resultObj => resultObj.id === obj.id))
+  }
+
+  t.falsy(result.find(obj => obj.id === 1).isNew)
+  t.true(result.find(obj => obj.id === 2).isNew)
+  t.true(result.find(obj => obj.id === 3).isNew)
+  t.true(result.find(obj => obj.id === 4).isNew)
+  t.falsy(result.find(obj => obj.id === 5).isNew)
+  t.falsy(result.find(obj => obj.id === 6).isNew)
 })

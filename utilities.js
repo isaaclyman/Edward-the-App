e422935a5c919a08ce2b17410bc53d0e232ecdcc
Isaac Lyman <isaaclyman@gmail.com>
2018-10-01 11:53:47 -0600

    Use UMD for shared utilities

--- utilities.js
+++ utilities.js
@@ -135,7 +135,17 @@ function getStyledArrayFromChapters(chapters) {
   return splitContent
 }
 
-module.exports = {
-  getStyledArrayFromChapters,
-  orderPromises,
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+      module.exports = factory();
+  } else {
+      root.returnExports = factory();
 }
+}(typeof self !== 'undefined' ? self : this, function () {
+  return {
+    getStyledArrayFromChapters,
+    orderPromises,
+  };
+}));

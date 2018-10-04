298181e570c7c7c4110ae42c48f09cbb3b81ea8e
Isaac Lyman <isaaclyman@gmail.com>
2018-10-03 21:38:38 -0600

    Fix service worker filename for PWA registration

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ Vue.config.productionTip = false
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceworker.js').then((registration) => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
       console.log('SW registered: ', registration)
     }).catch((registrationError) => {
       console.log('SW registration failed: ', registrationError)

b4b1ec1b60786bf7dce6de6a8f8ce52d2ab3ea01
Isaac Lyman <isaaclyman@gmail.com>
2018-10-13 07:53:55 -0600

    Put Jest tests in jest/ so it isn't confused with tests/

--- jest.config.js
+++ jest.config.js
@@ -136,8 +136,8 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "<rootDir>/test/**/*.js",
-    "!<rootDir>/test/**/_*.js"
+    "<rootDir>/jest/**/*.js",
+    "!<rootDir>/jest/**/_*.js"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped

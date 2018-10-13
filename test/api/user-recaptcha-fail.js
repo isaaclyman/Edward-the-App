01da7a2df21fa8a7de4ef57ee6e5ccb2b67fe90d
Isaac Lyman <isaaclyman@gmail.com>
2018-10-12 21:14:23 -0600

    Remove sinon from the project

--- test/api/user-recaptcha-fail.js
+++ test/api/user-recaptcha-fail.js
@@ -1,18 +1,16 @@
 import { app, route, serverReady } from '../_imports'
 import request from 'request-promise-native'
-import sinon from 'sinon'
 
-const sandbox = sinon.sandbox.create()
 const user = { email: 'trash@edwardtheapp.com', password: 'thisismysecurepassword', captchaResponse: 'token' }
 
 beforeAll(() => {
-  sandbox.stub(request, 'post')
-  .withArgs(sinon.match({ uri: 'https://www.google.com/recaptcha/api/siteverify' }))
-  .resolves({ success: false })
-})
-
-afterAll(() => {
-  sandbox.restore()
+  const oldPost = request.post
+  jest.spyOn(request, 'post').mockImplementation(body => {
+    if (body.uri === 'https://www.google.com/recaptcha/api/siteverify') {
+      return Promise.resolve({ success: false })
+    }
+    return oldPost.apply([...arguments])
+  })
 })
 
 test('does not log in when captcha fails', async () => {
